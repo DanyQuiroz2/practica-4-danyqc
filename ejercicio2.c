513563d66573eae6ad5fc32a547c03ca35f06e13
DanyQuiroz2 <dqc220618@gmail.com>
2025-03-06 14:16:40 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,20 @@
+//Daniela Quiroz Cruz
+//ejercicio 1
+ 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+    {
+        printf("a es mayor que\n");
+    }
+    else
+    {
+        printf("b es mayor \n");
+    }
+    return 0;
+}
