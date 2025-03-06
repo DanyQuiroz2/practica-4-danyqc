12d1648d243a16efaf254b688caca7a767b1f8a0
DanyQuiroz2 <dqc220618@gmail.com>
2025-03-06 14:20:43 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -18,3 +18,23 @@ int main()
     }
     return 0;
 }
+//Daniela Quiroz Cruz
+//ejercicio 1
+ 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a=3;
+    int b=4;
+    if(a>b)
+    {
+        printf("a es mayor que\n");
+    }
+    else
+    {
+        printf("b es mayor o igual a \n");
+    }
+    return 0;
+}
